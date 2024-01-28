3eada5c8d724721aaf78150b2c519f4bcf163dee
竹竹 <103553374+yingchu1313@users.noreply.github.com>
2024-01-28 15:24:45 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import helloWorld from "@/components/HelloWorld.vue";
+  import { ref, onMounted } from "vue";
+
+  const count = ref(0);
+
+  // 使用語法糖導入prop
+  const props = defineProps({
+    data: {
+      type: Object,
+      default: () => ({}),
+    }
+  })
+
+  const increment = () => {
+    count.value += 1;
+  };
+
+  onMounted(() => {
+    console.log(count.value)
+  });
+    
+</script>
+
+<template>
+  <div>
+    <h1>Vue:{{ count }}</h1>
+    <button @click="increment">Count is: {{ count }}</button>
+    <helloWorld></helloWorld>
+  </div>
+</template>
+
+<style scoped>
+</style>
